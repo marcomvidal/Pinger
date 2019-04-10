efed95a2b950b86da98659d8671d21428fd91331
Marco M. Vidal <marcomvidal@gmail.com>
2019-04-09 22:55:37 -0300

    Unit Tests Added

---
+++ Test/AttemptTest.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Pinger.Models;
+using System;
+using System.Net;
+
+namespace Test
+{
+    [TestClass]
+    public class AttemptTest
+    {
+        [TestMethod]
+        public void SetDefaultDestination_RevertsToDefault()
+        {
+            // Arrange
+            var expected = IPAddress.Parse("8.8.8.8");
+            var attempt = new Attempt();
+
+            // Act
+            attempt.Destination = IPAddress.Parse("4.4.4.4");
+            attempt.SetDefaultDestination();
+            var actual = attempt.Destination;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [DataRow("8.8.8.8")]
+        [DataRow("192.168.0.0")]
+        [DataRow("192.168.0.1")]
+        [DataRow("192.168.0.255")]
+        [DataRow("127.0.0.1")]
+        [DataRow("0")]
+        public void ValidateAddress_ItsAParseableAddress(string address)
+        {
+            // Arrange
+            var expected = IPAddress.Parse(address);
+            var attempt = new Attempt();
+            var exposedAttempt = new PrivateObject(attempt);
+
+            // Act
+            var actual = (IPAddress)exposedAttempt.Invoke(
+                "ValidateAddress",
+                address);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [DataRow("999.999.999.999")]
+        [DataRow("ABC")]
+        [DataRow("AB12CD34")]
+        [DataRow("")]
+        [DataRow(" ")]
+        public void ValidateAddress_IsAnInvalidAddress(string address)
+        {
+            // Arrange
+            var attempt = new Attempt();
+            var exposedAttempt = new PrivateObject(attempt);
+
+            // Act
+            var actual = (IPAddress)exposedAttempt.Invoke(
+                "ValidateAddress",
+                address);
+
+            // Assert
+            Assert.IsNull(actual);
+        }
+    }
+}
